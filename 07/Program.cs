95e45f5664780db0023cdf81aac7365053d2ba22
l.serra <l.serra@white3.it>
2025-12-09 11:55:08 +0100

    solved day 7 part 1

--- 07/Program.cs
+++ 07/Program.cs
@@ -1,7 +1,5 @@
 using System.Numerics;
-using System.Text;
 using Common;
-using Models;
 using Utils;
 
 namespace Day_07
@@ -34,10 +32,39 @@ namespace Day_07
 
         private static void PartOneCount(string input)
         {
+            var lines = input.Split('\n');
+            Logger.Log($"{lines[0]}");
+            for (var l = 1; l < lines.Length; l++)
+            {
+                var line = lines[l];
+                var ca = line.ToCharArray();
+                for (var c = 0; c < line.Length; c++)
+                {
+                    if (lines[l - 1][c] == 'S')
+                    {
+                        ca[c] = '|';
+                    }
+                    if (lines[l][c] == '^' && lines[l - 1][c] == '|')
+                    {
+                        if (c > 0)
+                            ca[c - 1] = '|';
+                        if (c < line.Length)
+                            ca[c + 1] = '|';
+                        _result++;
+                    }
+                    if (lines[l][c] == '.' && lines[l - 1][c] == '|')
+                    {
+                        ca[c] = '|';
+                    }
+                }
+                lines[l] = new string(ca);
+                Logger.Log($"{lines[l]}");
+            }
         }
 
         private static void PartTwoCount(string input)
         {
+            
         }
     }
 }
