a20e8e65977e7308330babad80f0066314647687
l.serra <l.serra@white3.it>
2025-12-17 17:35:09 +0100

    wip on 9.2

--- 09/MovieTheater.cs
+++ 09/MovieTheater.cs
@@ -1,5 +1,5 @@
+using System.Drawing;
 using System.Numerics;
-using System.Runtime.CompilerServices;
 using Common;
 using Model;
 using Utils;
@@ -43,6 +43,36 @@ namespace Day_09
             return area;
         }
 
+
+        private static bool IsCrossing(Point2D a1, Point2D a2, Point2D b1, Point2D b2)
+        {
+            var result = false;
+            if (a1.X == a2.X)
+            {
+                if (b1.X <= a1.X && b2.X >= a1.X && b1.Y <= a1.Y && b2.Y >= a2.Y)
+                    result = true;
+            }
+
+            return result;
+        }
+
+        private static bool PerimeterCheck(Point2D a, Point2D b)
+        {
+            var result = false;
+
+            var pointC = new Point2D(a.X, b.Y);
+            var pointD = new Point2D(b.X, a.Y);
+            var square = new List<Point2D> { a, pointC, b, pointD };
+
+            for (int i = 0, j = square.Count - 1; i < square.Count; j = i++)
+            {
+                Logger.Log($"[IsPointInPolygon] {i} {j}");
+
+            }
+
+            return result;
+        }
+
         private static void PartOneCount()
         {
             _result = 0;
@@ -53,12 +83,30 @@ namespace Day_09
                     var area = GetArea(_points[i], _points[j]);
                     if (area > _result)
                         _result = area;
-                }          
+                }
             }
         }
 
         private static void PartTwoCount()
         {
+            _resultTwo = 0;
+            for (var i = 0; i < _points.Count - 1; i++)
+            {
+                for (var j = i + 1; j < _points.Count; j++)
+                {
+                    Logger.Log($"  points: {_points[i]} {_points[j]}");
+
+                    if (PerimeterCheck(_points[i], _points[j]))
+                    {
+                        var area = GetArea(_points[i], _points[j]);
+                        if (area > _resultTwo)
+                        {
+                            Logger.Log($"    -> area {area} for points: {_points[i]} {_points[j]}");
+                            _resultTwo = area;
+                        }
+                    }
+                }
+            }
         }
     }
 }
--- obj/Debug/net9.0/AOC2025.AssemblyInfo.cs
+++ obj/Debug/net9.0/AOC2025.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AOC2025")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eba45fbc1f01fd21dacf6714938744a2e9015d0b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2c14683b168ce2f1ddf2e2ceedd560a421523ba")]
 [assembly: System.Reflection.AssemblyProductAttribute("AOC2025")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AOC2025")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
